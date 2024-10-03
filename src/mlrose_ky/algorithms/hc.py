1e3a361938c532e29b31a1dbcae3e79fce7fe71c
Kyle Nakamura <knakamura13dev@gmail.com>
2024-10-03 05:14:09 -0700

    tests: 100% coverage for .algorithms.hc; REMOVE RESTARTS PARAM
    The `restarts` parameter was removed from the `hill_climb` function to align it with the standard hill climbing algorithm, which does not involve random restarts. This restores the distinction between standard hill climbing and randomized hill climbing (RHC), where the latter handles random restarts.
    
    Key changes:
    - Removed the `restarts` parameter and related logic.
    - Simplified the initialization of the optimization problem to run without multiple restarts.
    - Adjusted callback handling, removing references to `current_restart`.
    - Updated the function's docstring to reflect the absence of the `restarts` parameter and emphasize standard hill climbing behavior.
    - Added tests to cover edge cases, including:
      - A test for early exit when `state_fitness_callback` returns `False` after 0 iterations.
      - A test for the standard callback functionality.
      - Edge case handling when no iterations are performed but the function returns a valid state and fitness.
    
    These changes ensure the `hill_climb` function adheres to its intended behavior, while the new tests provide full coverage for callback-related scenarios.

--- src/mlrose_ky/algorithms/hc.py
+++ src/mlrose_ky/algorithms/hc.py
@@ -1,4 +1,4 @@
-"""Functions to implement the randomized optimization and search algorithms."""
+"""Implementation of the Hill Climbing optimization algorithm."""
 
 # Authors: Genevieve Hayes (modified by Andrew Rollings, Kyle Nakamura)
 # License: BSD 3-clause
@@ -14,66 +14,78 @@ from mlrose_ky.decorators import short_name
 def hill_climb(
     problem: Any,
     max_iters: int | float = np.inf,
-    restarts: int = 0,
     init_state: np.ndarray = None,
     curve: bool = False,
     random_state: int = None,
     state_fitness_callback: Callable = None,
     callback_user_info: Any = None,
 ) -> tuple[np.ndarray, float, np.ndarray | None]:
-    """Use standard hill climbing to find the optimum for a given optimization problem.
+    """
+    Use standard hill climbing to find the optimum for a given optimization problem.
 
     Parameters
     ----------
     problem: optimization object
-        Object containing fitness function optimization problem to be solved.
-        For example, `DiscreteOpt()`, `ContinuousOpt()` or `TSPOpt()`.
-    max_iters: int, default: np.inf
-        Maximum number of iterations of the algorithm for each restart.
-    restarts: int, default: 0
-        Number of random restarts.
+        Object containing the optimization problem to be solved.
+        For example, `DiscreteOpt()`, `ContinuousOpt()`, or `TSPOpt()`.
+    max_iters: int or float, default: np.inf
+        Maximum number of iterations before the algorithm terminates.
+        Must be a positive integer greater than 0, or `np.inf`.
     init_state: np.ndarray, default: None
-        1-D Numpy array containing starting state for algorithm.
-        If None, then a random state is used.
+        1-D Numpy array containing the starting state for the algorithm.
+        If `None`, then a random state is used.
     curve: bool, default: False
-        Boolean to keep fitness values for a curve.
-        If False, then no curve is stored.
-        If True, then a history of fitness values is provided as a
+        Whether to keep fitness values for a curve.
+        If `False`, then no curve is stored.
+        If `True`, then a history of fitness values is provided as a
         third return value.
     random_state: int, default: None
-        If random_state is a positive integer, random_state is the seed used
-        by np.random.seed(); otherwise, the random seed is not set.
-    state_fitness_callback: callable, default: None
-        If specified, this callback will be invoked once per iteration.
-        Parameters are (iteration, current best state, current best fit, user callback data).
-        Return true to continue iterating, or false to stop.
+        Seed for the random number generator.
+    state_fitness_callback: Callable, default: None
+        If specified, this callback function is invoked once per iteration with
+        the following parameters:
+
+        - iteration: int
+        - attempt: None
+        - done: bool
+        - state: np.ndarray
+        - fitness: float
+        - curve: np.ndarray or None
+        - user_data: any
+
+        The callback should return a boolean: `True` to continue iterating,
+        or `False` to stop.
     callback_user_info: any, default: None
-        User data passed as last parameter of callback.
+        User data passed as the last parameter of the callback function.
 
     Returns
     -------
     best_state: np.ndarray
-        Numpy array containing state that optimizes the fitness function.
+        Numpy array containing the state that optimizes the fitness function.
     best_fitness: float
-        Value of fitness function at best state.
+        Value of the fitness function at the best state.
     fitness_curve: np.ndarray
-        Numpy array containing the fitness at every iteration.
-        Only returned if input argument curve is True.
+        Numpy array of shape (n_iterations, 2), where each row contains:
+
+        - Adjusted fitness at the current iteration.
+        - Cumulative number of fitness evaluations.
+
+        Only returned if the input argument `curve` is `True`.
+
+    Notes
+    -----
+    The `state_fitness_callback` function is also called before the optimization
+    loop starts (iteration 0) with the initial state and fitness values.
 
     References
     ----------
     Russell, S. and P. Norvig (2010). *Artificial Intelligence: A Modern Approach*, 3rd edition.
     Prentice Hall, New Jersey, USA.
     """
-    # TODO: fix and uncomment these problematic raise statements
-    # if not (isinstance(max_iters, int) or max_iters == np.inf or max_iters.is_integer()) or max_iters < 0:
-    #     raise ValueError(f"max_iters must be a positive integer. Got {max_iters}")
-    # if not (isinstance(restarts, int) or restarts.is_integer()) or restarts < 0:
-    #     raise ValueError(f"restarts must be a positive integer. Got {restarts}")
-    # if init_state is not None and len(init_state) != problem.get_length():
-    #     raise ValueError(
-    #         f"init_state must have the same length as the problem. " f"Expected length {problem.get_length()}, got {len(init_state)}"
-    #     )
+    if not (isinstance(max_iters, int) or max_iters == np.inf or max_iters.is_integer()) or max_iters < 0:
+        raise ValueError(f"max_iters must be a positive integer. Got {max_iters}")
+    if init_state is not None and len(init_state) != problem.get_length():
+        raise ValueError(f"init_state must have the same length as the problem. Expected {problem.get_length()}, got {len(init_state)}")
 
     # Set random seed
     if isinstance(random_state, int) and random_state > 0:
@@ -85,67 +97,70 @@ def hill_climb(
     fitness_curve = []
     best_fitness_curve = []
 
-    continue_iterating = True
-    for current_restart in range(restarts + 1):
-        # Initialize optimization problem
-        if init_state is None:
-            problem.reset()
-        else:
-            problem.set_state(init_state)
+    # Initialize optimization problem
+    if init_state is None:
+        problem.reset()
+    else:
+        problem.set_state(init_state)
 
-        callback_extra_data = None
+    # Initial callback invocation
+    if state_fitness_callback is not None:
+        continue_iterating = state_fitness_callback(
+            iteration=0,
+            attempt=None,
+            done=False,
+            state=problem.get_state(),
+            fitness=problem.get_adjusted_fitness(),
+            curve=np.asarray(fitness_curve) if curve else None,
+            user_data=callback_user_info,
+        )
+        if not continue_iterating:
+            return problem.get_state(), best_fitness, np.asarray(best_fitness_curve) if curve else None
+
+    # Main optimization loop
+    iters = 0
+    while iters < max_iters:
+        iters += 1
+        problem.current_iteration += 1
+
+        # Find neighbors and determine best neighbor
+        problem.find_neighbors()
+        next_state = problem.best_neighbor()
+        next_fitness = problem.eval_fitness(next_state)
+
+        # If curve is True, append current fitness and evaluations to fitness_curve
+        if curve:
+            fitness_curve.append((problem.get_adjusted_fitness(), problem.fitness_evaluations))
+
+        # Invoke callback
         if state_fitness_callback is not None:
-            callback_extra_data = callback_user_info + [("current_restart", current_restart)]
-            # initial call with base data
-            state_fitness_callback(
-                iteration=0, state=problem.get_state(), fitness=problem.get_adjusted_fitness(), user_data=callback_extra_data
+            max_attempts_reached = (iters == max_iters) or problem.can_stop()
+            continue_iterating = state_fitness_callback(
+                iteration=iters,
+                attempt=None,
+                done=max_attempts_reached,
+                state=problem.get_state(),
+                fitness=problem.get_adjusted_fitness(),
+                curve=np.asarray(fitness_curve) if curve else None,
+                user_data=callback_user_info,
             )
-        iters = 0
-        while iters < max_iters:
-            iters += 1
-
-            # Find neighbors and determine best neighbor
-            problem.find_neighbors()
-            next_state = problem.best_neighbor()
-            next_fitness = problem.eval_fitness(next_state)
-
-            if curve:
-                fitness_curve.append((problem.get_adjusted_fitness(), problem.fitness_evaluations))
-
-            # invoke callback
-            if state_fitness_callback is not None:
-                max_attempts_reached = (iters == max_iters) or problem.can_stop()
-                continue_iterating = state_fitness_callback(
-                    iteration=iters,
-                    attempt=None,
-                    done=max_attempts_reached,
-                    state=problem.get_state(),
-                    fitness=problem.get_adjusted_fitness(),
-                    curve=np.asarray(fitness_curve) if curve else None,
-                    user_data=callback_extra_data,
-                )
-                # break out if requested
-                if not continue_iterating:
-                    break
-
-            # If best neighbor is an improvement, move to that state
-            current_fitness = problem.get_fitness()
-            if next_fitness > current_fitness:
-                problem.set_state(next_state)
-            else:
+            # Break out if requested
+            if not continue_iterating:
                 break
 
-        # Update best state and best fitness
-        if problem.get_fitness() > best_fitness:
-            best_fitness = problem.get_fitness()
-            best_state = problem.get_state()
-            if curve:
-                best_fitness_curve = [*fitness_curve]
-                fitness_curve = []
-
-        # break out if requested
-        if not continue_iterating:
+        # If best neighbor is an improvement, move to that state
+        current_fitness = problem.get_fitness()
+        if next_fitness > current_fitness:
+            problem.set_state(next_state)
+        else:
             break
+
+    # Update best state and best fitness
+    if problem.get_fitness() > best_fitness:
+        best_fitness = problem.get_fitness()
+        best_state = problem.get_state()
+        if curve:
+            best_fitness_curve = [*fitness_curve]
 
     best_fitness = problem.get_maximize() * best_fitness
 
--- tests/test_algorithms/test_hc.py
+++ tests/test_algorithms/test_hc.py
@@ -4,8 +4,9 @@
 # License: BSD 3-clause
 
 import numpy as np
+import pytest
 
-from mlrose_ky import DiscreteOpt, OneMax, ContinuousOpt
+from mlrose_ky import DiscreteOpt, ContinuousOpt, OneMax
 from mlrose_ky.algorithms import hill_climb
 from tests.globals import SEED
 
@@ -16,28 +17,28 @@ class TestHillClimb:
     def test_hill_climb_discrete_max(self):
         """Test hill_climb function for a discrete maximization problem"""
         problem = DiscreteOpt(5, OneMax())
-        best_state, best_fitness, _ = hill_climb(problem, restarts=10, random_state=SEED)
+        best_state, best_fitness, _ = hill_climb(problem, random_state=SEED)
         x = np.ones(5)
         assert np.array_equal(best_state, x) and best_fitness == 5
 
     def test_hill_climb_continuous_max(self):
         """Test hill_climb function for a continuous maximization problem"""
         problem = ContinuousOpt(5, OneMax())
-        best_state, best_fitness, _ = hill_climb(problem, restarts=10, random_state=SEED)
+        best_state, best_fitness, _ = hill_climb(problem, random_state=SEED)
         x = np.ones(5)
         assert np.array_equal(best_state, x) and best_fitness == 5
 
     def test_hill_climb_discrete_min(self):
         """Test hill_climb function for a discrete minimization problem"""
         problem = DiscreteOpt(5, OneMax(), maximize=False)
-        best_state, best_fitness, _ = hill_climb(problem, restarts=10, random_state=SEED)
+        best_state, best_fitness, _ = hill_climb(problem, random_state=SEED)
         x = np.zeros(5)
         assert np.array_equal(best_state, x) and best_fitness == 0
 
     def test_hill_climb_continuous_min(self):
         """Test hill_climb function for a continuous minimization problem"""
         problem = ContinuousOpt(5, OneMax(), maximize=False)
-        best_state, best_fitness, _ = hill_climb(problem, restarts=10, random_state=SEED)
+        best_state, best_fitness, _ = hill_climb(problem, random_state=SEED)
         x = np.zeros(5)
         assert np.array_equal(best_state, x) and best_fitness == 0
 
@@ -47,3 +48,80 @@ class TestHillClimb:
         x = np.zeros(5)
         best_state, best_fitness, _ = hill_climb(problem, max_iters=1, init_state=x, random_state=SEED)
         assert best_fitness == 1
+
+    def test_hill_climb_callback_stop_at_iteration_0(self):
+        """Test hill_climb where the callback stops the iteration at 0."""
+        problem = DiscreteOpt(5, OneMax())
+        init_state = np.zeros(5)
+
+        # Define a callback function that stops after 0 iterations
+        # noinspection PyMissingOrEmptyDocstring
+        def callback_function(iteration, attempt, done, state, fitness, curve, user_data):
+            return False
+
+        # Call hill_climb with the callback function
+        best_state, best_fitness, fitness_curve = hill_climb(
+            problem, init_state=init_state, random_state=SEED, state_fitness_callback=callback_function
+        )
+
+        # Assert that no iterations were performed (i.e., algorithm stopped immediately)
+        assert problem.current_iteration == 0
+        assert np.array_equal(best_state, init_state)  # Since no iterations happened, the best state is the initial state
+        assert best_fitness == -np.inf  # Since no iterations happened, the best fitness should remain the initial value
+        assert fitness_curve is None  # Since curve is False by default
+
+    def test_hill_climb_invalid_max_iters(self):
+        """Test that hill_climb raises ValueError when max_iters is invalid."""
+        problem = DiscreteOpt(5, OneMax())
+        max_iters = -1
+        with pytest.raises(ValueError, match=f"max_iters must be a positive integer. Got {max_iters}"):
+            hill_climb(problem, max_iters=max_iters, random_state=SEED)
+
+    def test_hill_climb_invalid_init_state_length(self):
+        """Test that hill_climb raises ValueError when init_state length is invalid."""
+        problem = DiscreteOpt(5, OneMax())
+        init_state = np.zeros(4)  # Incorrect length
+        with pytest.raises(
+            ValueError, match=f"init_state must have the same length as the problem. Expected {problem.get_length()}, got {len(init_state)}"
+        ):
+            hill_climb(problem, init_state=init_state, random_state=SEED)
+
+    def test_hill_climb_with_callback(self):
+        """Test hill_climb with a state_fitness_callback."""
+        problem = DiscreteOpt(5, OneMax())
+
+        # noinspection PyMissingOrEmptyDocstring
+        def callback_function(iteration, attempt, done, state, fitness, curve, user_data):
+            user_data["iterations"].append(iteration)
+            return True  # Continue iterating
+
+        callback_data = {"iterations": []}
+        hill_climb(problem, random_state=SEED, state_fitness_callback=callback_function, callback_user_info=callback_data)
+
+        # Assert that the callback was called
+        assert len(callback_data["iterations"]) > 0
+
+    def test_hill_climb_callback_stop(self):
+        """Test hill_climb where the callback stops the iteration."""
+        problem = DiscreteOpt(5, OneMax())
+        init_state = np.zeros(5)
+
+        # noinspection PyMissingOrEmptyDocstring
+        def callback_function(iteration, attempt, done, state, fitness, curve, user_data):
+            if iteration >= 5:
+                return False  # Stop iterating
+            else:
+                return True  # Continue iterating
+
+        # Since the initial state is very sub-optimal, a better state will certainly be found, so iterations will increase
+        hill_climb(problem, init_state=init_state, random_state=SEED, state_fitness_callback=callback_function)
+
+        # Assert that the algorithm stopped at iteration 5
+        assert problem.current_iteration == 5
+
+    def test_hill_climb_with_curve(self):
+        """Test hill_climb function with curve=True."""
+        problem = DiscreteOpt(5, OneMax())
+        best_state, best_fitness, fitness_curve = hill_climb(problem, curve=True, random_state=SEED)
+        assert fitness_curve is not None
+        assert len(fitness_curve) > 0
